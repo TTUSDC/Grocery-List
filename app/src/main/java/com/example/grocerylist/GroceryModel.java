0e7a47906386b56c3dcfa0f080dd317ae77ac1d9
dlowrey <dane.r.lowrey@gmail.com>
2017-10-30 14:36:12 -0500

    Commented GroceryModel

--- app/src/main/java/com/example/grocerylist/GroceryModel.java
+++ app/src/main/java/com/example/grocerylist/GroceryModel.java
@@ -8,24 +8,31 @@ import java.util.ArrayList;
 
 public class GroceryModel {
 
+    // instance of our GroceryModel object
     private static GroceryModel instance;
+    // mock database to hold grocery list items
     private ArrayList<String> groceryList = new ArrayList<String>();
 
-    private GroceryModel() {
+    /* private constructor (so other classes can not create a new instance of GroceryModel */
+    private GroceryModel() {}
 
-    }
-
+    /*
+    public method to get an instance of Grocery model.
+    if one is already created, get that one, else, create a new one
+     */
     public static synchronized GroceryModel getInstance() {
-        if (instance == null) {
-            instance = new GroceryModel();
+        if (instance == null) {  // check if GroceryModel has already been instantiated
+            instance = new GroceryModel();  // instantiate new
         }
-        return instance;
+        return instance;  // return instance
     }
 
+    /* method to add a grocery list item to the mock database */
     public void add(String item) {
         groceryList.add(item);
     }
 
+    /* method to get all grocery list items from the mock database */
     public ArrayList<String> getAll() {
         return groceryList;
     }
